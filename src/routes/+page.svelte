89deafa3859f0df263e4adcaf1da5d289d15b67b
aster <137767097+aster-void@users.noreply.github.com>
2025-10-18 17:05:31 +0900

    chore: format code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import * as icons from "$lib/icons/index.js";
 
   let code = $state(
-`<!DOCTYPE html>
+    `<!DOCTYPE html>
 <html>
 <body>
   <h1>Life Game</h1>
@@ -10,77 +10,64 @@
     console.log('実行されました');
   <\/script>
 </body>
-</html>`);
+</html>`,
+  );
 
   let previewDoc = $derived(code);
   let showEditor = $state(true);
-
 </script>
 
 <div class="navbar bg-[#E0E0E0] shadow-sm">
   <div class="mx-5 avatar w-8 rounded">
-    <img
-      src="prepare_it_later"
-      alt="ut.code();_Logo" />
+    <img src="prepare_it_later" alt="ut.code();_Logo" />
   </div>
-  
+
   <div class="font-semibold text-black text-[20px]">Life code</div>
 
   <div class="btn btn-ghost btn-circle hover:bg-[rgb(220,220,220)] swap ml-20">
-    <img
-      class="size-6"
-      src={icons.LeftArrow}
-      alt="Left Arrow" />
+    <img class="size-6" src={icons.LeftArrow} alt="Left Arrow" />
   </div>
 
   <label class="btn btn-ghost btn-circle hover:bg-[rgb(220,220,220)] swap ml-5">
     <input type="checkbox" />
-    <img
-      class="size-6 swap-on" 
-      src={icons.Play}
-      alt="Play" />
-    <img
-      class="size-6 swap-off"
-      src={icons.Pause}
-      alt="Pause" />
+    <img class="size-6 swap-on" src={icons.Play} alt="Play" />
+    <img class="size-6 swap-off" src={icons.Pause} alt="Pause" />
   </label>
 
   <div class="btn btn-ghost btn-circle hover:bg-[rgb(220,220,220)] swap ml-5">
-    <img
-      class="size-6"
-      src={icons.RightArrow}
-      alt="Right Arrow" />
+    <img class="size-6" src={icons.RightArrow} alt="Right Arrow" />
   </div>
 
   <label class="swap ml-auto mr-5">
     <input type="checkbox" bind:checked={showEditor} />
     <div class="text-black">
-      <img
-        class="size-6"
-        src={icons.CodeBracket}
-        alt="Code Bracket" />
+      <img class="size-6" src={icons.CodeBracket} alt="Code Bracket" />
     </div>
   </label>
 </div>
 
 <div class="flex h-screen box-border">
-  <div class={[
-    "flex overflow-hidden bg-[rgb(202,202,202)] shrink-0 transition-[flex-basis] duration-300 ease-in-out",
-    showEditor ? 'basis-[60%]' : 'basis-full']}>
-    <iframe 
-      srcdoc={previewDoc} 
-      title="Preview" 
+  <div
+    class={[
+      "flex overflow-hidden bg-[rgb(202,202,202)] shrink-0 transition-[flex-basis] duration-300 ease-in-out",
+      showEditor ? "basis-[60%]" : "basis-full",
+    ]}
+  >
+    <iframe
+      srcdoc={previewDoc}
+      title="Preview"
       sandbox="allow-scripts"
       class="w-[90%] h-[80%] rounded-lg m-auto shadow-lg"
     ></iframe>
   </div>
 
-  <div class={[
-    "flex box-border bg-white shrink-0 overflow-hidden transition-[flex-basis,opacity] duration-300 ease-in-out",
-    showEditor ? 'basis-[40%] opacity-100' : 'basis-0 opacity-0']}>
-    <textarea 
-      bind:value={code} 
-      class="w-full h-full border-none p-4 font-mono bg-black text-[#0f0]"
+  <div
+    class={[
+      "flex box-border bg-white shrink-0 overflow-hidden transition-[flex-basis,opacity] duration-300 ease-in-out",
+      showEditor ? "basis-[40%] opacity-100" : "basis-0 opacity-0",
+    ]}
+  >
+    <textarea bind:value={code} class="w-full h-full border-none p-4 font-mono bg-black text-[#0f0]"
     ></textarea>
   </div>
 </div>
